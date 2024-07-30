37584e8ab5d09321ac85d3f080dd61ff63de1d4b
ARYAN VERMA <aryanver6@gmail.com>
2024-07-30 22:13:11 +0530

    30/07/2024

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
+import "./Components/Styles/GlobalStyle.css";
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import React, { useState } from 'react'
+
 
 function App() {
+
+  let [mode , setMode] = useState('light'); 
+
+  let togglemode = ()=>{
+    if(mode=== 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#2f2e2e'
+      document.body.style.color = 'white'
+    }
+
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white'
+      document.body.style.color = 'black'
+    }
+  }
+
   return (
     <>
-    <Navbar title="TextUtils" />
-    <TextForm/>
+    <Navbar title="TextUtils" mode={mode} togglemode={togglemode} />
+    <TextForm mode={mode}/>
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-export default function TextForm() {
+export default function TextForm(props) {
 
     const handleUpClick = () => {
         let newText = text.toUpperCase();
@@ -41,7 +41,7 @@ export default function TextForm() {
                 <h3>Enter Your Text To Ananlyze</h3>
             </div>
             <div className="container form-floating my-3">
-                <textarea className="form-control" placeholder="Leave a comment here" value={text} onChange={handleOnChange} id="floatingTextarea" style={{ height: 200 + "px" }}></textarea>
+                <textarea className="form-control" placeholder="Leave a comment here" style={{backgroundColor: props.mode==='dark'?'#2f2e2e':'white', color : props.mode==='dark'?'white':'black' ,  height: 200 + "px" }} value={text} onChange={handleOnChange} id="floatingTextarea"></textarea>
             </div>
             <div className='container my-2'>
                 <button type="button" className="btn btn-primary mx-2" onClick={handleUpClick}>To Uppercase</button>
